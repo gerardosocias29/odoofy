68408ffe8c0c5ea25cb2e853ee921d4885e995ac
Gerardo <g.socias29@gmail.com>
2025-06-30 23:05:32 +0800

    fix: enhance product retrieval logic to fallback on variant_id if shopify_product_id is not found

--- models/shopify_sync.py
+++ models/shopify_sync.py
@@ -1607,6 +1607,10 @@ class ShopifySync(models.Model):
             product = self.env['product.product'].sudo().search([
                 ('shopify_id', '=', str(shopify_product_id))
             ], limit=1)
+        if not product and variant_id:
+            product = self.env['product.product'].sudo().search([
+                ('shopify_variant_id', '=', str(variant_id))
+            ], limit=1)
 
         # log product
         if product:
